e79577409f3969f7812a0e58475ea1c78b2f9e24
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2022-10-20 15:32:21 +0200

    Basic Layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+import deno from "@astrojs/deno";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: deno({
+    port: 8000,
+    hostname: "0.0.0.0"
+  }),
+  integrations: [tailwind()]
+});
