b34ed0b6f9079608fcad8a02b9fd84cb57cbbd0d
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2023-03-08 21:45:12 +0100

    Add infinite loop to start files

--- run.bat
+++ run.bat
@@ -1,3 +1,7 @@
+:loop
+
 set HOST=0.0.0.0
 set PORT=4565
 node ./dist/server/entry.mjs
+
+goto loop
--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-HOST=0.0.0.0 PORT=4565 node ./dist/server/entry.mjs
+while true
+do
+    HOST=0.0.0.0 PORT=4565 node ./dist/server/entry.mjs
+    sleep 1
+done
