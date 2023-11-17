86094d696b1fc780273c1266b5850068fe8d8ce0
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2023-11-17 16:31:58 +0100

    Update run and start scripts

--- run.bat
+++
@@ -1,7 +0,0 @@
-:loop
-
-set HOST=0.0.0.0
-set PORT=1701
-node ./dist/server/entry.mjs
-
-goto loop
--- run.sh
+++
@@ -1,5 +0,0 @@
-while true
-do
-    HOST=0.0.0.0 PORT=1701 node ./dist/server/entry.mjs
-    sleep 1
-done
